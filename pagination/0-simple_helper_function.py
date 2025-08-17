0ad09fb4d1f849f401350440ee795a2bddaa1b05
JCCallisto <jeancarrion8@gmail.com>
2025-08-17 17:37:13 -0400

    Correcting pycodestyle

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -6,6 +6,7 @@ This module provides a helper function for pagination.
 
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Returns a tuple of start and end index for a pagination range.
