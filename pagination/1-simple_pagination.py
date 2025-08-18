a74a79c95983363ebb2fbd21ae985959420ba8d6
JCCallisto <jeancarrion8@gmail.com>
2025-08-17 21:21:02 -0400

    Pycode corrections

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -24,6 +24,7 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     end: int = page * page_size
     return (start, end)
 
+
 class Server:
     """Server class to paginate a database of popular baby names.
     """
