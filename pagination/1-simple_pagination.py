44d89fd598b7fad94be295caba6842f07621b01f
JCCallisto <jeancarrion8@gmail.com>
2025-08-17 20:26:00 -0400

    Pagination

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+"""
+This module contains a Server class for simple pagination of a dataset.
+"""
+
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Returns a tuple of start and end index for a pagination range.
+
+    Args:
+        page (int): The current page number (1-indexed).
+        page_size (int): The number of items per page.
+
+    Returns:
+        Tuple[int, int]: A tuple containing the start and end index.
+    """
+    start: int = (page - 1) * page_size
+    end: int = page * page_size
+    return (start, end)
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+
+        Returns:
+            List[List]: The cached dataset, omitting the header row.
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Returns a page of the dataset.
+
+        Args:
+            page (int): The current page number (1-indexed).
+            page_size (int): The number of items per page.
+
+        Returns:
+            List[List]: A list of rows for the requested page.
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        dataset = self.dataset()
+        start, end = index_range(page, page_size)
+        if start >= len(dataset):
+            return []
+        return dataset[start:end]
