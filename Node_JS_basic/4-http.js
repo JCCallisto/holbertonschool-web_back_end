b7ac92267ac00935d65c3aa5b4e1334beb558f70
JCCallisto <jeancarrion8@gmail.com>
2025-09-05 21:20:09 -0400

    Http

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+/**
+ * Creates a small HTTP server.
+ */
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(1245);
+
+module.exports = app;
